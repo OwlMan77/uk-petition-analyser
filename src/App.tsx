5c411b3ad8ab99dba1c7e13a047d6422f34a6394
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2025-07-04 01:11:12 +0100

    feat: first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Search from './components/Search/Search';
+import ViewPetition from './features/Petition/Petition';
+
 
 function App() {
-
+  const [id, setId] = useState('');
   return (
     <>
+    <Search onSearch={setId}/>
+    {
+      id === '' ?
+      (<div> Please enter a valid petition id, (as found on <a target='_blank' href='https://petition.parliament.uk/'>https://petition.parliament.uk/</a> ) </div>)
+      :
+      <ViewPetition id={id} />
+    }
     </>
   )
 }
 
-export default App
+export default App;
